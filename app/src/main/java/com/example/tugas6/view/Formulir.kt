4b9e66523f13af567473ae82a2312dcd8028458c
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-06 13:12:03 +0700

    Tambah struktur dasar Scaffold dengan TopAppBar

--- app/src/main/java/com/example/tugas6/view/Formulir.kt
+++ app/src/main/java/com/example/tugas6/view/Formulir.kt
@@ -2,7 +2,10 @@
 
 package com.example.tugas6.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 
 @Composable
@@ -10,6 +13,13 @@ fun FormIsian(
     jenisK:List<String> = listOf("Laki-laki", "Perempuan")
     OnSubmitBtnClick : () -> Unit
 ){
+    Scaffold (
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
 
+            )
+        }
+    ){  }
 }
 
