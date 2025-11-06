23f5db666afdc01056002dbe3b52777db0296a2a
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-06 12:17:21 +0700

    Menambahkan struktur Scaffold dan NavHost untuk navigasi antar layar

--- app/src/main/java/com/example/tugas6/Navigasi.kt
+++ app/src/main/java/com/example/tugas6/Navigasi.kt
@@ -1,6 +1,8 @@
 package com.example.tugas6
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 enum class Navigasi {
     Formulirku,
@@ -9,6 +11,26 @@ enum class Navigasi {
 }
 
 @Composable
-fun DataApp(){
-    navController
+fun DataApp(
+    navController : NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian(
+                    // pilihanJK = JenisK.map { id -> resources.getString(id) }
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
+
+
 }
