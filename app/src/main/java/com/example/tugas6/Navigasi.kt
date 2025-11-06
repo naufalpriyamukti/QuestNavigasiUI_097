2c93fd0617097f896a1d94458410bbc4e71eb922
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-06 12:30:05 +0700

    Memperbaiki erro dengan menambah import Navigasi.kt

--- app/src/main/java/com/example/tugas6/Navigasi.kt
+++ app/src/main/java/com/example/tugas6/Navigasi.kt
@@ -1,8 +1,11 @@
 package com.example.tugas6
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
 
 enum class Navigasi {
     Formulirku,
