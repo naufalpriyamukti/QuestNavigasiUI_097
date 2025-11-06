12799001a1e10e6b104f50599a73c1203e903312
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-06 13:29:37 +0700

    Tambah column dan nilai parameternya

--- app/src/main/java/com/example/tugas6/view/Formulir.kt
+++ app/src/main/java/com/example/tugas6/view/Formulir.kt
@@ -2,13 +2,21 @@
 
 package com.example.tugas6.view
 
+import android.R.attr.text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.tugas6.R
 
 
 @Composable
@@ -20,12 +28,16 @@ fun FormIsian(
         modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = {text(stringResource(id=R.string.home),
+                title = {text(stringResource(id= R.string.home),
                     color = Color.White)},
                 colors = TopAppBarDefaults.topAppBarColors
                     (containerColor = colorResource(id = R.color.teal_700))
             )
         }
-    ){  }
+    ){
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally){  }
+    }
 }
 
