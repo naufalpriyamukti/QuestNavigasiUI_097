e37cea90fc2a6f601c36ccc75cecaf781e0aed40
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-06 14:11:27 +0700

    Finish

--- app/src/main/java/com/example/tugas6/MainActivity.kt
+++ app/src/main/java/com/example/tugas6/MainActivity.kt
@@ -20,10 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Tugas6Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    DataApp(modifier = Modifier)
                 }
             }
         }
