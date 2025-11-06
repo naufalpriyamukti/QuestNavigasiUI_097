cb109ac4f50b7d681aa8900f2deea852fd4a00ad
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-06 13:51:37 +0700

    Tambah function dan variabel items

--- app/src/main/java/com/example/tugas6/view/TampilData.kt
+++ app/src/main/java/com/example/tugas6/view/TampilData.kt
@@ -1,2 +1,19 @@
 package com.example.tugas6.view
 
+import androidx.annotation.experimental.Experimental
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick: () ->Unit
+){
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama" ),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya" ),
+        Pair(first = stringResource(id = "ALAMAT"), second = "Yogyakarta" )
+    )
+}
